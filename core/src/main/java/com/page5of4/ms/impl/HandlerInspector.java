5867c22c6cee8cbd6d0d411befe7a5aca6803176
Jacob Lewallen <jlewallen@gmail.com>
2012-06-09 15:13:48 -0600

    Inspecting handlers for bound methods, some specs.

---
+++ core/src/main/java/com/page5of4/ms/impl/HandlerInspector.java
@@ -0,0 +1,81 @@
+package com.page5of4.ms.impl;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.page5of4.ms.MessageHandler;
+
+public class HandlerInspector {
+   public HandlerDescriptor discoverBindings(Object object) {
+      return new HandlerDescriptor(object.getClass());
+   }
+
+   public static class HandlerDescriptor {
+      private final Class<?> handlerType;
+      private final List<String> problems = new ArrayList<String>();
+      private final List<HandlerBinding> bindings = new ArrayList<HandlerBinding>();
+
+      public HandlerDescriptor(Class<?> handlerType) {
+         super();
+         this.handlerType = handlerType;
+         for(Method method : handlerType.getMethods()) {
+            if(method.getAnnotation(MessageHandler.class) != null) {
+               Class<?> messageType = getMessageTypeFromParameters(method);
+               if(messageType != null) {
+                  bindings.add(new HandlerBinding(handlerType, messageType, method));
+               }
+            }
+         }
+         if(bindings.isEmpty()) {
+            problems.add(String.format("Class '%s' has no handler methods.", handlerType.getName()));
+         }
+      }
+
+      private Class<?> getMessageTypeFromParameters(Method method) {
+         Class<?>[] parameterTypes = method.getParameterTypes();
+         for(Class<?> type : parameterTypes) {
+            return type;
+         }
+         problems.add(String.format("Method '%s::%s' has no valid message parameters.", handlerType.getName(), method.getName()));
+         return null;
+      }
+
+      public List<String> getProblems() {
+         return problems;
+      }
+
+      public Class<?> getHandlerType() {
+         return handlerType;
+      }
+
+      public List<HandlerBinding> getBindings() {
+         return bindings;
+      }
+   }
+
+   public static class HandlerBinding {
+      private final Class<?> handlerType;
+      private final Class<?> messageType;
+      private final Method method;
+
+      public Class<?> getHandlerType() {
+         return handlerType;
+      }
+
+      public Class<?> getMessageType() {
+         return messageType;
+      }
+
+      public Method getMethod() {
+         return method;
+      }
+
+      public HandlerBinding(Class<?> handlerType, Class<?> messageType, Method method) {
+         super();
+         this.handlerType = handlerType;
+         this.messageType = messageType;
+         this.method = method;
+      }
+   }
+}
---
+++ core/src/test/java/com/page5of4/ms/MessageHandlerSpecs.java
@@ -0,0 +1,65 @@
+package com.page5of4.ms;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import org.junit.Test;
+
+import com.page5of4.ms.impl.HandlerInspector;
+import com.page5of4.ms.impl.HandlerInspector.HandlerDescriptor;
+
+public class MessageHandlerSpecs {
+   private HandlerInspector inspector = new HandlerInspector();
+
+   @Test
+   public void when_handler_has_zero_handler_methods() {
+      HandlerDescriptor descriptor = inspector.discoverBindings(new EmptyHandler());
+      assertThat(descriptor.getProblems()).containsOnly("Class 'com.page5of4.ms.MessageHandlerSpecs$EmptyHandler' has no handler methods.");
+      assertThat(descriptor.getBindings()).isEmpty();
+   }
+
+   @Test
+   public void when_handler_has_invalid_handler_method() {
+      HandlerDescriptor descriptor = inspector.discoverBindings(new BadHandlerMethodHandler());
+      assertThat(descriptor.getProblems()).containsOnly("Method 'com.page5of4.ms.MessageHandlerSpecs$BadHandlerMethodHandler::handle' has no valid message parameters.", "Class 'com.page5of4.ms.MessageHandlerSpecs$BadHandlerMethodHandler' has no handler methods.");
+      assertThat(descriptor.getBindings()).isEmpty();
+   }
+
+   @Test
+   public void when_handler_has_one_handler_methods() {
+      HandlerDescriptor descriptor = inspector.discoverBindings(new OneHandler());
+      assertThat(descriptor.getProblems()).isEmpty();
+      assertThat(descriptor.getBindings().size()).isEqualTo(1);
+      assertThat(descriptor.getBindings().get(0).getMessageType()).isEqualTo(String.class);
+   }
+
+   @Test
+   public void when_handler_has_two_handler_methods() {
+      HandlerDescriptor descriptor = inspector.discoverBindings(new TwoHandlers());
+      assertThat(descriptor.getProblems()).isEmpty();
+      assertThat(descriptor.getBindings().size()).isEqualTo(2);
+   }
+
+   @MessageHandler
+   public static class EmptyHandler {}
+
+   @MessageHandler
+   public static class BadHandlerMethodHandler {
+      @MessageHandler
+      public void handle() {}
+   }
+
+   @MessageHandler
+   public static class OneHandler {
+      @MessageHandler
+      public void handle(String message) {}
+   }
+
+   @MessageHandler
+   public static class TwoHandlers {
+      @MessageHandler
+      public void handle(String message) {}
+
+      @MessageHandler
+      public void handle(Long message) {}
+   }
+}

490c4024c6514d2b6c7751db6201f17ec1f69842
Jacob Lewallen <jlewallen@gmail.com>
2012-07-03 15:39:14 -0700

    Use an existing Component name as a template.

--- core/src/main/java/com/page5of4/codon/camel/CodonComponentResolver.java
+++ core/src/main/java/com/page5of4/codon/camel/CodonComponentResolver.java
@@ -1,8 +1,8 @@
 package com.page5of4.codon.camel;
 
-import org.apache.activemq.camel.component.ActiveMQComponent;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Component;
+import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.impl.DefaultComponentResolver;
 import org.apache.camel.spi.ComponentResolver;
 import org.slf4j.Logger;
@@ -12,6 +12,7 @@ import com.page5of4.codon.impl.TransactionConvention;
 
 public class CodonComponentResolver implements ComponentResolver {
    private static final Logger logger = LoggerFactory.getLogger(CodonComponentResolver.class);
+   private static String TEMPLATE_COMPONENT_NAME = "activemq";
    private final DefaultComponentResolver resolver = new DefaultComponentResolver();
    private final TransactionConvention transactionConvention;
 
@@ -21,14 +22,13 @@ public class CodonComponentResolver implements ComponentResolver {
 
    @Override
    public Component resolveComponent(String name, CamelContext camelContext) throws Exception {
-      logger.info("Preparing for communication with {}", name);
-
-      ActiveMQComponent component = new ActiveMQComponent();
-      component.setCamelContext(camelContext);
-      component.setBrokerURL("tcp://127.0.0.1:61616");
-      component.setTransactionManager(transactionConvention.locate(name, component.getConfiguration().getConnectionFactory()));
-      component.setTransacted(true);
-      return component;
+      logger.info("Resolving '{}'", name);
+      Component resolved = resolver.resolveComponent(TEMPLATE_COMPONENT_NAME, camelContext);
+      if(resolved instanceof JmsComponent) {
+         JmsComponent jmsComponent = (JmsComponent)resolved;
+         jmsComponent.setTransactionManager(transactionConvention.locate(name, jmsComponent.getConfiguration().getConnectionFactory()));
+         jmsComponent.setTransacted(true);
+      }
+      return resolved;
    }
-
 }

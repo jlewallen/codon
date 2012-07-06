3950f836d8fb8b4d7ed8b7c609a493890c41f851
Jacob Lewallen <jlewallen@gmail.com>
2012-07-06 16:57:18 -0700

    Spring XML integration.

---
+++ extender/src/main/java/com/page5of4/codon/extender/spring/CodonOsgiNamespaceHandler.java
@@ -0,0 +1,10 @@
+package com.page5of4.codon.extender.spring;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+public class CodonOsgiNamespaceHandler extends NamespaceHandlerSupport {
+   @Override
+   public void init() {
+
+   }
+}
